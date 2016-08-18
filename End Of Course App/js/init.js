8ed4195b247786e4f159ded841e252dbb176ab90
sjefferies83 <sjefferies83@googlemail.com>
2016-08-18 20:45:12 +0100

    End Of JavaScript Course
    Commit of finished application from JavaScript Course

---
+++ End Of Course App/js/init.js
@@ -0,0 +1,13 @@
+/**
+ * Created by jefferies on 18/08/2016.
+ */
+"use strict";
+
+var searchButton = document.querySelector("button");
+var searchCity = document.querySelector("#city");
+
+var loadingText = document.querySelector("#load");
+var weatherBox = document.querySelector("#weather");
+var weatherCity = weatherBox.firstElementChild;
+var weatherDescription = document.querySelector("#weatherDescription");
+var weatherTemperature = weatherBox.lastElementChild;
