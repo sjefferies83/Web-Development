45b6d9a3b750dfde3f0366122a8587ebc288f378
sjefferies83 <sjefferies@local.brunelcare.org.uk>
2016-08-19 14:49:45 +0100

    New Story App
    Started story app

---
+++ Story/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by sjefferies on 19/08/2016.
+ */
+
+var characterName = document.querySelector("#name");
+var characterSex = document.querySelector("#sex");
+var monster = document.querySelector("#monster");
+var weapon = document.querySelector("#weapon");
+var storyButton = document.querySelector("#generateButton;")
