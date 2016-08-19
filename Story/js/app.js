2b62d8a85d9feab73aed8f56adf7e36ae5129d1b
sjefferies83 <sjefferies@local.brunelcare.org.uk>
2016-08-19 16:04:49 +0100

    Story Edit
    Added Labels

--- Story/js/app.js
+++ Story/js/app.js
@@ -6,4 +6,8 @@ var characterName = document.querySelector("#name");
 var characterSex = document.querySelector("#sex");
 var monster = document.querySelector("#monster");
 var weapon = document.querySelector("#weapon");
-var storyButton = document.querySelector("#generateButton;")
+var storyButton = document.querySelector("#generateButton;");
+
+function createStory(){
+
+}
